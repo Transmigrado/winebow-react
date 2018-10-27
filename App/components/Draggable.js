c2f772faa15064296c5885c032b2240adeb55b2d
Jorge Acosta <transmigrado@gmail.com>
2018-10-26 22:58:13 -0300

    add draggable view

---
+++ App/components/Draggable.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react"
+import {
+  StyleSheet,
+  PanResponder,
+  Animated,
+  Text
+} from "react-native";
+
+import Card from './Card'
+
+export default class Draggable extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pan: new Animated.ValueXY()
+    };
+  }
+
+  componentWillMount() {
+  
+    
+    this.state.pan.addListener((value) => this._val = value);
+   
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (e, gesture) => true,
+      onPanResponderMove: Animated.event([
+        null, { dy: this.state.pan.y }
+      ]),
+      onPanResponderRelease: (e, gesture) => {
+        Animated.spring(this.state.pan, {
+          toValue: { x: 0, y: 0 },
+          friction: 5
+        }).start();
+      }
+      //this.state.pan.setValue({ x:0, y:0})
+    })
+  }
+
+  render() {
+    const panStyle = {
+      transform: this.state.pan.getTranslateTransform()
+    }
+    return (
+        <Animated.View
+          {...this.panResponder.panHandlers}
+          style={panStyle}
+        >
+            <Card>
+                <Text>Regiones</Text>
+            </Card>
+        </Animated.View>
+    );
+  }
+}
+
+
--- App/screens/MainScreen.js
+++ App/screens/MainScreen.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react'
 import { StyleSheet, View} from 'react-native'
 import Mapbox from '@mapbox/react-native-mapbox-gl'
+import Draggable from '../components/Draggable'
 
 Mapbox.setAccessToken('pk.eyJ1IjoidHJhbnNtaWdyYWRvIiwiYSI6InZaSDVNVk0ifQ.XbzDhB01GxzIm44_FlvyFQ')
-
-export default class App extends Component {
-  render() {
-    return <View style={styles.container}>
-       <Mapbox.MapView
+/*
+<Mapbox.MapView
             zoomLevel={10}
             centerCoordinate={[11.256, 43.770]}
             style={styles.container}>
         </Mapbox.MapView>
+*/
+export default class App extends Component {
+  render() {
+    return <View style={styles.container}>
+        <Draggable />
     </View>
   }
 }
