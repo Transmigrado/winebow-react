d7b107d23a2e0192dd1828d6ca21770333ca0959
Jorge Acosta <transmigrado@gmail.com>
2018-10-27 22:12:01 -0300

    fix cocoa Pod

---
+++ App/components/Pager.js
@@ -0,0 +1,67 @@
+import {StyleSheet, View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {PagerTabIndicator, IndicatorViewPager, PagerTitleIndicator, PagerDotIndicator} from 'rn-viewpager';
+
+export default class Pager extends Component {
+    render() {
+        return (
+            <View style={{flex:1}}>
+                <IndicatorViewPager
+                    style={{height:200}}
+                    indicator={this._renderDotIndicator()}
+                >
+                    <View style={{backgroundColor:'cadetblue'}}>
+                        <Text>page one</Text>
+                    </View>
+                    <View style={{backgroundColor:'cornflowerblue'}}>
+                        <Text>page two</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1AA094'}}>
+                        <Text>page three</Text>
+                    </View>
+                </IndicatorViewPager>
+
+                <IndicatorViewPager
+					style={{flex:1, paddingTop:20, backgroundColor:'white'}}
+                    indicator={this._renderTitleIndicator()}
+                >
+                    <View style={{backgroundColor:'cadetblue'}}>
+                        <Text>page one</Text>
+                    </View>
+                    <View style={{backgroundColor:'cornflowerblue'}}>
+                        <Text>page two</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1AA094'}}>
+                        <Text>page three</Text>
+                    </View>
+                </IndicatorViewPager>
+                
+                <IndicatorViewPager
+					style={{flex:1, paddingTop:20, backgroundColor:'white'}}
+                 
+                >
+                    <View style={{backgroundColor:'cadetblue'}}>
+                        <Text>page one</Text>
+                    </View>
+                    <View style={{backgroundColor:'cornflowerblue'}}>
+                        <Text>page two</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1AA094'}}>
+                        <Text>page three</Text>
+                    </View>
+                </IndicatorViewPager>
+            </View>
+        );
+    }
+
+    _renderTitleIndicator() {
+        return <PagerTitleIndicator titles={['one', 'two', 'three']} />;
+    }
+
+    _renderDotIndicator() {
+        return <PagerDotIndicator pageCount={3} />;
+    }
+    
+    
+
+}
