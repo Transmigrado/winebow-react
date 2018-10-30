eff678e4cd77d989bf2c82df6656ca162dc417dc
transmigrado <transmigrado@gmail.com>
2018-10-30 16:27:56 -0300

    add HTML Parser, for description

--- App/screens/WineScreen.js
+++ App/screens/WineScreen.js
@@ -9,7 +9,9 @@ import Breadcump from '../components/Breadcump'
 import WineyardItem from '../components/Item/WineyardItem'
 import PropTypes from 'prop-types'
 import WineItem from '../components/Item/WineItem'
+import parser  from 'react-native-html-parser'
 
+const DomParser = parser.DOMParser
 
 class WineScreen extends Component {
 
@@ -30,14 +32,18 @@ renderItem = ({item, index}) => {
 renderHeader = ()=>{
   
   const { navigation, path } = this.props;
-  const item = navigation.getParam('item', {});
+  const item = navigation.getParam('item', {})
+
+  const doc = new DomParser().parseFromString(item.description,'text/html')
+
+  console.log(doc)
 
   return  <View>
        <Pager />
     <Breadcump path={path} style={{margin:10}} />
         <WineyardItem bigTitle={true} item={item} />
         <View style={{paddingHorizontal: 20, marginTop: 10}}>
-        <Text>{item.description.replace('<p>','').replace('</p>','').replace('&nbsp;','')}</Text>
+        <Text>hola</Text>
         <Text style={{fontSize: 30,marginTop:10, fontWeight:'bold'}}>Wines</Text>
         </View>
         
